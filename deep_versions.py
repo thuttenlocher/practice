474a92b4a3a6053f51fc6e2623e72a93535b7e57
Tyler <huttenlochert@gmail.com>
2020-02-29 12:47:44 -0600

    Create deep_versions.py

---
+++ deep_versions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 23 18:39:09 2019
+
+@author: Tyler H
+"""
+
+# theano
+import theano
+print('theano: %s' % theano.__version__)
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
