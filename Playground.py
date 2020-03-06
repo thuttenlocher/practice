af9ea7242534d7c01fff4b6b7d02bf045889a70d
Tyler <huttenlochert@gmail.com>
2020-03-05 19:17:09 -0600

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -5,13 +5,7 @@ Created on Thu Feb 27 21:12:10 2020
 @author: Tyler H
 """
 
-basecapturerate = 0.33
-cpmultiplier = 0.75
-ball, curve, berry, throw, medal = 1, 1, 1, 1, 1
-baserate = (1 - (basecapturerate / (2 * cpmultiplier)))
-multipliers = ball * curve * berry * throw * medal
-probability = 1 - baserate ** multipliers
-print(round(probability, 2))
+print(float(False))
 
 
 
