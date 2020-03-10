1312ebe8483114c4a3799761567999193cb42758
Tyler <huttenlochert@gmail.com>
2020-03-10 17:42:14 -0500

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -5,75 +5,38 @@ Created on Thu Feb 27 21:12:10 2020
 @author: Tyler H
 """
 
-item = "quesadilla"
-meat = "pork"
-queso = True
-guacamole = True
-double_meat = False
-#-----------------------------------------------------------
-#Let's further expand our previous program to cover a broader
-#menu variety. Instead of just burritoes, now the program
-#should cover three menu items: quesadillas, burritoes, and
-#nachos. Instead of separate booleans for steak and pork,
-#we instead have a string that could be "steak", "pork",
-#"chicken", "tofu", and "beef". We still have booleans for
-#queso and guacamole, but we also have a boolean for double
-#meat.
+
+
+
+
+entered = "abc123"
+password = "abc123"
+tries = 3
+
+#Above we've created three variables representing an attempt
+#to enter a password. Add some code below that will print the
+#result of this check:
 #
-#Your code should calculate the price as follows:
+# - "Login successful." if entered is the same as password
+#   and tries is less than or equal to 3.
+# - "Incorrect password." if entered is not the same as 
+#   password, but tries is less than or equal to 3.
+# - "Tries exceeded." if tries is greater than 3.
 #
-# - The base price for a quesadilla is 4.00, for nachos is
-#   4.50, and for burritoes is 5.00.
-# - If meat is steak or pork, add 0.50. Any other meat adds
-#   no money to the price.
-# - guacamole always adds 1.00 to the price.
-# - queso adds 1.00 to the price UNLESS the item is nachos,
-#   in which case it adds nothing.
-# - double_meat adds 1.50 if the meat is steak or pork, or
-#   1.00 otherwise.
+#You don't need to worry about incrementing tries if the
+#password is incorrect.
 
-base_price = 4.5
-if item == "quesadilla":
-    base_price = 4.0
-    print("ordered quesadilla")
-elif item == "burrito":
-    base_price = 5.0
-    print("ordered burrito")
+
+if entered == password and tries <=3:
+    print("Login successful.")
 else:
-    base_price += 0.0
-    print("ordered nachos")
-
-    
-if meat == "pork":
-    base_price += 0.50
-    print("ordered pork")
-elif meat == "steak":
-    base_price += 0.50
-    print("ordered steak")
-else:
-    base_price += 0.00
-    print("didn't order steak or pork")
-
-    
-if meat == ("steak" or "pork") and double_meat:
-    base_price += 1.50
-    print("ordered steak or pork and double_meat")
-elif double_meat:
-    base_price += 1.0
-    print("ordered double meat not of steak or pork")
+    if entered != password and tries <=3:
+        print("Incorrect password.")
+    else:
+        print("Tries exceeded.")
 
 
-if guacamole:
-    base_price += 1.0
-    print("ordered guacamole")
 
-if queso and item != "nachos":
-    base_price += 1.0
-    print("ordered nachos")
-
-    
-    
-print(base_price)
 
 
 
