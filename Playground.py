7ccf2e4e2670bd82ee5ba57cb42703be8c73218b
TylerHoot <huttenlochert@gmail.com>
2020-03-12 21:08:21 -0500

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -8,25 +8,11 @@ Created on Thu Feb 27 21:12:10 2020
 
 
 
-mystery_int = 5
 
-#In math, factorial is a mathematical operation where an
-#integer is multipled by every number between itself and 1.
-#For example, 5 factorial is 5 * 4 * 3 * 2 * 1, or 120.
-#Factorial is represented by an exclamation point: 5!
-#
-#Use a for loop to calculate the factorial of the number
-#given by mystery_int above. Then, print the result.
-#
-#Hint: Running a loop from 1 to mystery_int will give you
-#all the integers you need to multiply together. You'll need
-#to track the total product using a variable declared before
-#starting the loop, though!
-num = 1
 
-for i in range(1, mystery_int + 1):
-    num = i * num
-print(num)
+for index in mystery_string:
+    print(index, mystery_string[index])
+
 
 
 
