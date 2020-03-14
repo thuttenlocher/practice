0d33ce60d0ce21335303f948fd9f042e2fb0e257
TylerHoot <huttenlochert@gmail.com>
2020-03-14 12:34:16 -0500

    Create 3.3.4 Excercise 1.py
    File Creation

---
+++ 3.3.4 Excercise 1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 14 12:03:18 2020
+
+@author: Tyler H
+"""
+
+
+
+mystery_value = 82
+
+
+#Write a while loop that continues to add 9 to mystery_value
+#until mystery_value is greater than 100. Each time 9 is
+#added, print the *new* value of mystery_value. For example,
+#with mystery_value = 87, your code should print 96 and 105.
+
+
+while mystery_value <= 100:
+        mystery_value += 9
+        print(mystery_value)
+
+
+
+
+
+
+
+
