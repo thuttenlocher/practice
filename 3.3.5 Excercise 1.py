e2cccc7832e62b9298047a05b8a5ffd82128777e
Tyler <huttenlochert@gmail.com>
2020-03-18 08:19:29 -0500

    Create 3.3.5 Excercise 1.py

---
+++ 3.3.5 Excercise 1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 20:45:37 2020
+
+@author: A6DB5ZZ
+"""
+
+count = 0
+for x in range(3):
+     for x in range(3):
+         count = count + x
+         print(count)
+         
+         
