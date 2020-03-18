e5cdec584ca244cc82d1bb0a71f952791728f5cb
Tyler <huttenlochert@gmail.com>
2020-03-17 20:30:40 -0500

    Create Interlude 3.3.2.py

---
+++ Interlude 3.3.2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 20:11:27 2020
+
+@author: A6DB5ZZ
+"""
+
+num = 5
+
+#Write some code below that will print the factorial of num.
+#Remember to remove any debug statements when you're ready
+#to submit for credit.
+
+result = 1
+for i in range(1, num + 1):
+#    print(i, result)
+    result *= i
+print(result)
+
+
+
+
