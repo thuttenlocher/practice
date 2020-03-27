8f9c8e7d2fe9f4400abf332ac7bf0bc2a65aa90b
Tyler <huttenlochert@gmail.com>
2020-03-26 19:06:49 -0500

    Create 3.4.4 Coding Excercise 2.py

---
+++ 3.4.4 Coding Excercise 2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 18:43:24 2020
+
+@author: A6DB5ZZ
+"""
+
+
+#Write a function that takes in one integer parameter, the
+#side length of a square, and returns the area. The function
+#should be named find_area, and have one parameter:
+#side_length.
+
+
+#Write your function here!
+def find_area(test_side_length):
+    return test_side_length**2
+
+
+#Below are some lines of code that will test your function.
+#You can change the value of the variable(s) to test your
+#function with different inputs.
+#
+#If your function works correctly, this will originally
+#print: "A square with side length 4 has an area of 16".
+
+test_side_length = 4
+print("A square with side length", test_side_length, "has an area of", find_area(test_side_length))
+
+
+
+
+
+
+
+
+
