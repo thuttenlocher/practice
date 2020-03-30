edb1c33012fe0b5cdb398fe1f9d3a0581c6e99e8
TylerHoot <huttenlochert@gmail.com>
2020-03-29 23:12:05 -0500

    Create 3.5.3 Coding Exercise 1.py

---
+++ 3.5.3 Coding Exercise 1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 29 23:09:49 2020
+
+@author: Tyler H
+"""
+
+mystery_value = "9"
+
+#You may modify the lines of code above, but don't move them!
+#When you Submit your code, we'll change these lines to
+#assign different values to the variables.
+
+#Create a program that divides 10 by mystery_value and prints
+#the result. In the case that an error occurs, print "Not
+#possible".
+#
+#Use error handling to determine if an error will occur; do
+#not use the type() function. You might be surprised how many
+#types Python can divide by 10!
+
+
+#Add your code here!
+
+try:
+    value = 10/mystery_value
+    print(value)
+    
+except: 
+    print("Not possible")
